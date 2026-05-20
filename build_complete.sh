8a88352e3f948442bdb4b81e7f436563121054b5
a1g <a1g0r1thm109110@gmail.com>
2026-05-19 21:48:41 -0400

    oop

--- build_complete.sh
+++ build_complete.sh
@@ -80,7 +80,7 @@ if [ "$(check_deps "$needed_deps")" ]; then
   #install deps automatically on debian, arch and ubuntu
   print_title "attempting to install build deps"
   if [ -f "/etc/debian_version" ]; then
-    apt-get install wget python3 unzip zip debootstrap cpio binwalk cgpt kmod pv lz4 cryptsetup -y
+    apt-get install wget python3 unzip zip debootstrap cpio binwalk cgpt kmod pv lz4 cryptsetup bsdtar -y
     if apt-cache show pcre2-utils 2>/dev/null; then
       apt-get install pcre2-utils -y
     else
@@ -88,7 +88,7 @@ if [ "$(check_deps "$needed_deps")" ]; then
     fi
     create_aliases
   elif [ "$(pacman --help)" ]; then
-    pacman -Syu --needed --noconfirm wget python3 unzip zip debootstrap cpio binwalk kmod pv lz4 cryptsetup
+    pacman -Syu --needed --noconfirm wget python3 unzip zip debootstrap cpio binwalk kmod pv lz4 cryptsetup bsdtar
     if [ "$(check_deps "cgpt")" ]; then
       #waiter waiter more unnecessary oneliners please
       command -v yay >/dev/null 2>&1 && sudo -u "$SUDO_USER" yay -S --noconfirm cgpt-bin || (command -v paru >/dev/null 2>&1 && sudo -u "$SUDO_USER" paru -S --noconfirm cgpt-bin) || { print_error "install an aur helper you freak"; exit 1; }
